6046c9a83dfe2953745279f8a855967ecd09c9e1
Itamar Afek <iafek@salesforce.com>
2017-11-29 14:11:33 -0800

    refactoring post_slack

--- prepare_env.sh
+++ prepare_env.sh
@@ -3,4 +3,4 @@
 virtualenv env || exit 1
 source env/bin/activate || exit 1
 
-pip install --no-deps -r pip-requirements.txt || exit 1
+pip install -r pip-requirements.txt || exit 1
--- push/post_slack.py
+++ push/post_slack.py
@@ -2,17 +2,28 @@
 import json
 import requests
 
-# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
-webhook_url = 'https://hooks.slack.com/services/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'
-slack_data = {'text': 'test posting from python'}
 
-response = requests.post(
-    webhook_url, data=json.dumps(slack_data),
-    headers={'Content-Type': 'application/json'}
-)
+class SlackPoster(object):
 
-if response.status_code != 200:
-    raise ValueError(
-        'Request to slack returned an error %s, the response is:\n%s'
-        % (response.status_code, response.text)
-    )
+	def __init__(self):
+		self.webhook_url = 'https://hooks.slack.com/services/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'
+
+	def post(self, message):
+
+		slack_data = {'text': message}
+
+		response = requests.post(
+			self.webhook_url, data=json.dumps(slack_data),
+			headers={'Content-Type': 'application/json'}
+		)
+
+		if response.status_code != 200:
+			raise ValueError(
+				'Request to slack returned an error %s, the response is:\n%s'
+				% (response.status_code, response.text)
+			)
+
+
+if __name__ == "__main__":
+	poster = SlackPoster()
+	poster.post('testing from python')
